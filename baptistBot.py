0205b5597738bb27afc8be866e45bc0bb8a9ca97
warthurii <67800188+warthurii@users.noreply.github.com>
2022-01-15 18:29:55 -0500

    splits status text into multiple string that fit twitters character length (#1)

--- baptistBot.py
+++ baptistBot.py
@@ -1,5 +1,7 @@
 import tweepy
 import os
+import json
+import re
 
 def createTwitterObj():
     API_KEY = str(os.getenv('BAPTIST_API_KEY'))
@@ -12,6 +14,40 @@ def createTwitterObj():
 
     return tweepy.API(auth)
 
+def getCatechismStatusText():
+    num = 0
+    with open('nums.json') as f1:
+        nums = json.load(f1)
+        num = nums['catechismNum']
+
+    with open('catechism.json', 'r') as catechism:
+        catechism = json.load(catechism)
+
+        q = catechism[num]['question']
+        a = catechism[num]['answer']
+        number = catechism[num]['number']
+
+        statusText = f'Q{number}: {q}\nA: {a}'
+
+    if(len(statusText) > 280):
+        words  = re.split('( )', statusText)
+        status = ['']
+        i = 0
+        length = 276
+        for word in words:
+            if(len(status[i]) + len(str(word)) < length):
+                status[i] += str(word)
+            else:
+                status.append(f'...{word}')
+                i += 1
+                length = 273
+        statusText = status
+    
+    with open('nums.json', 'w') as f2:
+        nums['catechismNum'] = num + 1
+        json.dump(nums, f2, indent=4)
+
+    return statusText        
+
 if __name__=="__main__":
-    tweeter = createTwitterObj()
-    tweeter.update_status('Hello\nWorld')
+    print(getCatechismStatusText())
